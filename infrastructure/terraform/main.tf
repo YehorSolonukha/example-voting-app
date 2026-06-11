188f5e7018a0be269357e0cb81a9fa80ff972be2
Yehor Solonukha <solonuhaegor@gmail.com>
2026-06-11 09:50:10 +0200

    updated infrastructure set up

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,4 +1,3 @@
-# Definicja dostawcy (Azure)
 terraform {
   required_providers {
     azurerm = {
@@ -12,27 +11,26 @@ provider "azurerm" {
   features {}
 }
 
-# 1. Tworzenie Grupy Zasobów (Resource Group) - kontenera na nasz klaster
 resource "azurerm_resource_group" "rg" {
   name     = "rg-voting-app-thesis"
-  location = "polandcentral" # Możesz zmienić na najbliższy region, np. polandcentral
+  location = "polandcentral"
 }
 
-# 2. Tworzenie klastra AKS
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-voting-cluster"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "votingapp-k8s"
 
-  # Konfiguracja puli węzłów (Node Pool)
+  oidc_issuer_enabled       = true
+  workload_identity_enabled = true
+
   default_node_pool {
     name       = "default"
-    node_count = 2                  # Dokładnie 2 węzły, aby udowodnić rozproszenie systemu
-    vm_size    = "Standard_D2s_v3"  # Optymalny rozmiar (2 vCPU, 8GB RAM) pod aplikacje i metryki
+    node_count = 2
+    vm_size    = "Standard_D2s_v3"
   }
 
-  # Przypisanie tożsamości zarządzanej (Managed Identity) dla klastra
   identity {
     type = "SystemAssigned"
   }
@@ -42,7 +40,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-# Wyjście (Output) - Terraform wypisze nam nazwę klastra po zakończeniu pracy
 output "kubernetes_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
